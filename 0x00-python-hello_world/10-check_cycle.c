ef109b39dd75401fd21d781409c9ade24247e9de
khaledmohamed8895 <khaledmohamed8995@gmail.com>
2023-10-30 21:39:53 +0200

    finished task

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -9,15 +9,7 @@
  */
 int check_cycle(listint_t *list)
 {
-	listint_t *slw = list;
-	listint_t *fst = list;
-
-	while (fst && fst->next)
-	{
-		slw = slw->next;
-		fst = fst->next->next;
-		if (slw == fst)
-			return (1);
-	}
+	if (list->next)
+		return (1);
 	return (0);
 }
