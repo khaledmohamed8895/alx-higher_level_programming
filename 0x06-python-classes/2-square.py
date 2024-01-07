58646cd1885e355379d4393b9c7efe8d732c455f
khaledmohamed8895 <khaledmohamed8995@gmail.com>
2024-01-07 19:15:17 +0200

    test solution

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -6,9 +6,9 @@ class Square:
     """Square"""
 
     def __init__(self, size=0):
-        if not isinstance(size, int):
-            raise Exception('size must be an integer')
+        if not type(size) == int:
+            raise Exception("size must be an integer")
         elif size < 0:
-            raise Exception('size must be >= 0')
+            raise Exception("size must be >= 0")
 
         self._Square__size = size
