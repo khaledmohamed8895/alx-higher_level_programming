5150120f2c0152650ec1bdf7485d6050c8185993
khaledmohamed8895 <khaledmohamed8995@gmail.com>
2024-01-08 22:10:54 +0200

    finished

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
-MyList = __import__("1-my_list").MyList
+'''Module for MyList class.'''
 
-my_list = MyList()
-my_list.append(1)
-my_list.append(4)
-my_list.append(2)
-my_list.append(3)
-my_list.append(5)
-print(my_list)
-my_list.print_sorted()
-print(my_list)
+
+class MyList(list):
+    '''Custom MyList class.'''
+    def print_sorted(self):
+        '''Method for printing sorted list.'''
+        print(sorted(self))
