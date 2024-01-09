bfd0c8e8162bae1291fe0cf62509079240fe58cb
khaledmohamed8895 <khaledmohamed8995@gmail.com>
2024-01-09 22:16:06 +0200

    finished

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Pascal."""
+
+
+def pascal_triangle(n):
+    """pascal_triangle
+
+    Args:
+        n (_type_): _description_
+
+    Returns:
+        list_t[:n]: list_t
+    """
+
+    if n <= 0:
+        return []
+
+    list_t = [[1], [1, 1]]
+    for i in range(2, n):
+        list_row = []
+        list_row.append(1)
+        list_row.append(1)
+        for j in range(1, i):
+            list_row.insert(j, list_t[i - 1][j] + list_t[i - 1][j - 1])
+        list_t.append(list_row)
+    return list_t[:n]
