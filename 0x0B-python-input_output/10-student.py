7c1a655dff35d55f809da008c574284b296f3c4a
khaledmohamed8895 <khaledmohamed8995@gmail.com>
2024-01-09 22:10:05 +0200

    finished

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Student Class"""
+
+
+class Student:
+    """Class Student that defines a student."""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+        dictionary = dict()
+        for element in attrs:
+            for key in self.__dict__:
+                if element == key:
+                    dictionary[element] = self.__dict__[key]
+        return dictionary
