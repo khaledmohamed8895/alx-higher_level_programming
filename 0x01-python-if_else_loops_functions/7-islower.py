849e8999bf3ed5e5811a71680439c61fb0aea30d
khaledmohamed8895 <khaledmohamed8995@gmail.com>
2024-02-29 17:55:20 +0200

    try new sulution change code

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-def islower(x):
-    a = ord(x)
-    if a >= 65 and a < 90:
+def islower(c):
+    if ord(c) in range(97,123):
+        return True 
+    else:
         return False
-    elif a >= 97 and a <= 122:
-        return True
