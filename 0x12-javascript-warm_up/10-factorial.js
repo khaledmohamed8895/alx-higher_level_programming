09b36e9da42b716a9c97bac917bfc1a28ea6571c
khaledmohamed8895 <khaledmohamed8995@gmail.com>
2024-03-11 21:14:12 +0200

    finished

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,12 +1,9 @@
 #!/usr/bin/node
-function factorial(n) {
-  if (n < 0) {
-    return (-1);
-  }
-  if (n === 0 || isNaN(n)) {
+function fact (a) {
+  if (!a) {
     return (1);
+  } else {
+    return a * fact(a - 1);
   }
-  return (n * factorial(n - 1));
 }
-
-console.log(factorial(Number(process.argv[2])));
+console.log(fact(process.argv[2]));
