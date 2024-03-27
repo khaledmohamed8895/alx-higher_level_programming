aa1e1475e101ab29982c65925afd0e99e3339bcd
khaledmohamed8895 <khaledmohamed8995@gmail.com>
2024-03-27 23:58:12 +0200

    finished task

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# bash script that takes in a URL, sends a get request to the URL,displays the body of the response
-curl -sL "$1"
+# Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response.
+curl -s "$1" | wc -c
