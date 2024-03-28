30be1d9a44849fe43c8a75f926fe2614b556ae74
khaledmohamed8895 <khaledmohamed8995@gmail.com>
2024-03-28 22:27:52 +0200

    upddating

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,17 +1,18 @@
-"""Find the peak"""
-
-
-def find_peak(arr):
-    """Return the peak with O(n)"""
-    if len(arr) == 0 or arr is None:
+#!/usr/bin/python3
+def find_peak(list_of_integers):
+    """......."""
+    if not list_of_integers:
         return None
 
-    peak = arr[0]
-    n = len(arr)
+    low = 0
+    high = len(list_of_integers) - 1
 
-    for i in range(n):
-        if i != 0 and i != n-1 and arr[i-1] <= arr[i] >= arr[i+1]:
-            if arr[i] > peak:
-                peak = arr[i]
+    while low < high:
+        mid = (low + high) // 2
 
-    return peak
+        if list_of_integers[mid] < list_of_integers[mid + 1]:
+            low = mid + 1
+        else:
+            high = mid
+
+    return list_of_integers[low]
