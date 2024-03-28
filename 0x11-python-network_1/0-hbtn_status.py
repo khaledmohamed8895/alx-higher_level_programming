86bb1aaa0abe1cdffbd7beae9f3520ba8e4b11fd
khaledmohamed8895 <khaledmohamed8995@gmail.com>
2024-03-28 21:42:59 +0200

    finished task

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,23 +1,11 @@
 #!/usr/bin/python3
-"""Fetching URL using urllib"""
-from urllib.request import urlopen
-
-
-def url_req(url):
-    """"
-    Requesting url
-    Arguments:
-        url: the url to request
-    """    
-    with urlopen(url) as res:
-        content = res.read()
-        html = content.decode("UTF-8")
-        print('Body response:')
-        print('\t- type: {}'.format(type(content)))
-        print('\t- content: {}'.format(content))
-        print('\t- utf8 content: {}'.format(html))
-
+"""Python script that fetches webpage"""
 
 if __name__ == "__main__":
-    url = "https://alx-intranet.hbtn.io/status"
-    url_req(url)
+    import urllib.request
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as r:
+        body = r.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode('utf-8')))
