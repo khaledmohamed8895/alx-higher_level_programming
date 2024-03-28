b773cac459c9702284c5f45d7fc7ae6cb95c7287
khaledmohamed8895 <khaledmohamed8995@gmail.com>
2024-03-28 21:52:48 +0200

    finished task2

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Printing the res or the error """
+
+
+if __name__ == "__main__":
+    from sys import argv
+    from urllib import request, error
+
+    try:
+        with request.urlopen(argv[1]) as r:
+            print(r.read().decode('UTF-8'))
+    except error.HTTPError as e:
+        print('Error code:', e.code)
